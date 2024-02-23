656d45fcd0e0b8634cbc5fbee4b64ad250bf38e5
hussien415532 <hussien415532@fci.bu.edu.eg>
2024-02-23 19:27:50 +0200

    kick

---
+++ src/app/Components/xcare/xcare.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-xcare',
+  templateUrl: './xcare.component.html',
+  styleUrls: ['./xcare.component.css']
+})
+export class XcareComponent {
+
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,25 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './Views/home/home.component';
+import { NavbarComponent } from './Components/navbar/navbar.component';
+import { LandingComponent } from './Components/landing/landing.component';
+import { XcareComponent } from './Components/xcare/xcare.component';
+import { AiComponent } from './Components/ai/ai.component';
+import { ContactComponent } from './Components/contact/contact.component';
+import { FooterComponent } from './Components/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    NavbarComponent,
+    LandingComponent,
+    XcareComponent,
+    AiComponent,
+    ContactComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
